28a3bfa7360934732618cb9415a18671ddd572cb
Varun Mamtora <varunmamtora@gmail.com>
2021-03-27 02:15:57 +0530

    add otp send feature

--- ekyc/main/views.py
+++ ekyc/main/views.py
@@ -79,9 +79,29 @@ def verify_phone(request):
     if request.method == 'POST':
         phone = request.POST['phone']
         print(phone)
+        import twilio
+        # Download the helper library from https://www.twilio.com/docs/python/install
+        from twilio.rest import Client
+        import random # generate random number
+        otp = random.randint(1000,9999)
+        OTP.objects.create(otp_code=otp, user=request.user)
+        print("Generated OTP is - ",otp)
+        # Your Account Sid and Auth Token from twilio.com/console
+        # DANGER! This is insecure. See http://twil.io/secure
+        account_sid = 'AC703679e4bfdc618b2c00d92b79be454c'
+        auth_token = '49fb345bb91b32322a28a510da05aa6e'
+        client = Client(account_sid, auth_token)
+
+        message = client.api.account.messages.create(
+                body='Hello Dear, ' + request.user.username +'Your Secure Device OTP is - ' + str(otp),
+                from_='+18102165640',
+                to='+91'+ phone
+            )
+
+        print(message.sid)
         return redirect('/')
     else:
-        return render(request, 'verify.html')
+        return render(request, 'phone.html')
 
 def verify_docs(request):
     if request.method == 'POST':
