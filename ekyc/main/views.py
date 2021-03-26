6a8d870863374d796b8794a29eb28232616de6d7
Varun Mamtora <varunmamtora@gmail.com>
2021-03-26 20:56:12 +0530

    add authentication

--- ekyc/main/views.py
+++ ekyc/main/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import models as userModel
 # Create your views here.
 
 def index(request):
@@ -8,3 +8,52 @@ def index(request):
 
 def verify(request):
     return render(request, 'verify.html')
+
+def register(request):
+    if request.method == 'POST':
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if userModel.User.objects.filter(username=username).exists():
+                print('Usernme exists')
+                messages.info(request, 'Username exists')
+                return redirect('register')
+            elif userModel.User.objects.filter(email=email).exists():
+                print('email exists')
+                messages.info(request, 'Email exists')
+                return redirect('register')
+            else:
+                user = userModel.User.objects.create_user(
+                    username=username, email=email, password=password1, first_name=fname, last_name=lname)
+                user.save()
+                return redirect('login')
+        else:
+            print('pass dosent match')
+            messages.info(request, 'Password didn\'t match')
+            return redirect('register')
+        return redirect('/')
+    else:
+        return render(request, "register.html")
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = userModel.auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            userModel.auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalid details')
+            return redirect('login')
+    else:
+        return render(request, "login.html")
+    return render(request, "login.html")
+
